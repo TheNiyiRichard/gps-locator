a9f4fac868834cf267a3cafdbe74e7da99c52f26
TheNiyiRichard <144950945+TheNiyiRichard@users.noreply.github.com>
2023-12-14 13:31:16 -0600

    completed

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Utility functions
-function onEvent(event, selector, callback) {
-    return selector.addEventListener(event, callback);
+function onEvent(element, event, callback) {
+    return element.addEventListener(event, callback);
 }
 
 function select(selector, parent = document) {
@@ -17,7 +17,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidGhlbG1hLWRldiIsImEiOiJjbGJncnJqc2wwaXhjM29xd
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    interractive: false,
+    interactive: false, // Corrected spelling
     center: [0, 0],
     pitch: 40,
     zoom: 16
@@ -31,36 +31,37 @@ map.touchZoomRotate.disable();
 
 const marker = new mapboxgl.Marker({
     color: '#3898ff'
-})
+});
 
 function getLocation(position) {
     const { longitude, latitude } = position.coords;
 
-    if(longitude && latitude) {
+    if (longitude && latitude) {
         map.setCenter([longitude, latitude]);
         marker.setLngLat([longitude, latitude]).addTo(map);
-        setTimeout(() => { overlay.style.display = 'none'}, 750);
+        setTimeout(() => {
+            overlay.style.display = 'none';
+        }, 750);
         loader.style.display = 'none';
     }
 }
 
 function errorHandler(event) {
     loader.style.animationPlayState = 'paused';
-    console.log(event.message);
+    console.error(event.message);
 }
 
 const options = {
     enableHighAccuracy: true,
-    maximumAge: 0       // To always get the users location at each step
-}
+    maximumAge: 0 // To always get the user's location at each step
+};
 
 /*
     The watchPosition() method is used to register a handler function that will 
     be called automatically each time the position of the device changes
 */
-
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition(getLocation, errorHandler, options);
 } else {
-    console.log('Geolocation is not supported by your browser');
+    console.error('Geolocation is not supported by your browser');
 }
